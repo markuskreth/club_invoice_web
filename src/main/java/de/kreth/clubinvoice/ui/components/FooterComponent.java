c181083801c6641de63d0a507e86ff4872187c4f
Markus Kreth <markus.kreth@web.de>
2018-07-20 23:51:21 +0200

    Loading version by FileReader, parsing Date with utc TimeZone

--- src/main/java/de/kreth/clubinvoice/ui/components/FooterComponent.java
+++ src/main/java/de/kreth/clubinvoice/ui/components/FooterComponent.java
@@ -1,7 +1,14 @@
 package de.kreth.clubinvoice.ui.components;
 
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Properties;
+import java.util.TimeZone;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -20,8 +27,13 @@ public class FooterComponent extends HorizontalLayout {
 	private final static Properties version = new Properties();
 	static {
 		try {
-			version.load(FooterComponent.class
-					.getResourceAsStream("/version.properties"));
+			String fileUrl = FooterComponent.class
+					.getResource("/hibernate.cfg.xml").getFile();
+			File file = new File(fileUrl);
+			File rootDir = file.getParentFile().getParentFile().getParentFile();
+			File propFile = new File(rootDir, "version.properties");
+
+			version.load(new FileReader(propFile));
 		} catch (IOException | NullPointerException e) {
 			LOGGER.error(
 					"Error loading version properties file: " + e.getMessage());
@@ -35,10 +47,22 @@ public class FooterComponent extends HorizontalLayout {
 
 		if (propertiesLoaded()) {
 
+			String dateTimeProperty = version.getProperty("build.dateTime");
+			SimpleDateFormat sourceFormat = new SimpleDateFormat(
+					"yyyy-MM-dd HH:mm:ss");
+			sourceFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+			try {
+				Date date = sourceFormat.parse(dateTimeProperty);
+				dateTimeProperty = DateFormat.getDateTimeInstance(
+						DateFormat.MEDIUM, DateFormat.SHORT).format(date);
+			} catch (ParseException e) {
+				LOGGER.warn(
+						"Unable to parse dateTimeProperty=" + dateTimeProperty,
+						e);
+			}
 			Label vers = new Label(
 					"Version: " + version.getProperty("project.version"));
-			Label buildTime = new Label(
-					"Build: " + version.getProperty("build.dateTime"));
+			Label buildTime = new Label("Build: " + dateTimeProperty);
 			addComponents(vers, buildTime);
 		}
 	}
