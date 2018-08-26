88bb8643f7cdb5e663d884e5d3ccbf31e4347e58
Markus Kreth <markus.kreth@web.de>
2018-08-26 19:55:48 +0200

    Show pdf in a vaadin window

--- src/main/java/de/kreth/clubinvoice/ui/components/InvoiceDialog.java
+++ src/main/java/de/kreth/clubinvoice/ui/components/InvoiceDialog.java
@@ -5,13 +5,22 @@ import static de.kreth.clubinvoice.ui.Constants.CAPTION_INVOICE_INVOICENO;
 import static de.kreth.clubinvoice.ui.Constants.LABEL_CANCEL;
 import static de.kreth.clubinvoice.ui.Constants.LABEL_STORE;
 
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import com.vaadin.server.StreamResource;
 import com.vaadin.shared.Registration;
+import com.vaadin.ui.AbstractComponent;
+import com.vaadin.ui.BrowserFrame;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
 import com.vaadin.ui.Button.ClickListener;
 import com.vaadin.ui.DateTimeField;
 import com.vaadin.ui.HorizontalLayout;
@@ -24,10 +33,10 @@ import de.kreth.clubinvoice.data.InvoiceItem;
 import de.kreth.clubinvoice.report.InvoiceReportSource;
 import net.sf.jasperreports.engine.JRException;
 import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.view.JasperViewer;
 
 public class InvoiceDialog extends Window {
 
@@ -54,7 +63,7 @@ public class InvoiceDialog extends Window {
 		okButton = new Button(resBundle.getString(LABEL_STORE), ev -> close());
 		Button cancel = new Button(resBundle.getString(LABEL_CANCEL),
 				ev -> close());
-		Button previewButton = new Button("Preview", ev -> showPdf());
+		Button previewButton = new Button("Preview", ev -> showPdf(ev));
 		HorizontalLayout btnLayout = new HorizontalLayout();
 		btnLayout.addComponents(okButton, cancel, previewButton);
 
@@ -68,20 +77,59 @@ public class InvoiceDialog extends Window {
 		setInvoice(invoice);
 	}
 
-	private void showPdf() {
+	private void showPdf(ClickEvent ev) {
+		try {
+			JasperPrint print = createJasperPrint();
+			showInWebWindow(print, ev);
+		} catch (JRException | IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
+	private void showInWebWindow(JasperPrint print, ClickEvent ev) throws IOException, JRException {
+
+	    Window window = new Window();
+	    window.setCaption("View PDF");
+	    AbstractComponent e = createEmbedded(print);
+	    window.setContent(e);
+	    window.setModal(true);
+	    window.setWidth("50%");
+	    window.setHeight("90%");
+
+	    ev.getButton().getUI().addWindow(window);
+	}
+
+	private AbstractComponent createEmbedded(JasperPrint print) throws IOException, JRException {
+
+		PipedInputStream in = new PipedInputStream();
+		final PipedOutputStream out = new PipedOutputStream(in);
+
+		final StreamResource resource = new StreamResource(() -> in, "invoice.pdf");
+	    resource.setMIMEType("application/pdf");
+	    
+		BrowserFrame c = new BrowserFrame("PDF invoice", resource);
+	    c.setSizeFull();
+	    
+	    ExecutorService exec = Executors.newSingleThreadExecutor();
+	    exec.execute(() -> {
+	    	try {
+				JasperExportManager.exportReportToPdfStream(print, out);
+			} catch (JRException e) {
+				throw new RuntimeException(e);
+			}
+	    });
+	    exec.shutdown();
+	    return c;
+	}
+
+	public JasperPrint createJasperPrint() throws JRException {
 		InvoiceReportSource source = new InvoiceReportSource();
 		source.setInvoice(invoice);
-
-		try {
-			JasperReport report = JasperCompileManager
-					.compileReport(getClass().getResourceAsStream(MTV_JRXML));
-			JasperPrint print = JasperFillManager.fillReport(report,
-					new HashMap<>(), source);
-			JasperViewer.viewReport(print, false);
-		} catch (JRException e) {
-			e.printStackTrace();
-		}
+		JasperReport report = JasperCompileManager
+				.compileReport(getClass().getResourceAsStream(MTV_JRXML));
+		JasperPrint print = JasperFillManager.fillReport(report,
+				new HashMap<>(), source);
+		return print;
 	}
 
 	public Registration addOkClickListener(ClickListener listener) {
