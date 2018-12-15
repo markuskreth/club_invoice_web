2499628a63955a7f13de8c9f138e7058b8b17c6d
Markus Kreth <markus.kreth@web.de>
2018-12-15 18:21:09 +0100

    Webclient Test by selenium

---
+++ src/test/java/de/kreth/clubinvoice/ui/tests/WebClientTests.java
@@ -0,0 +1,53 @@
+package de.kreth.clubinvoice.ui.tests;
+
+import java.sql.SQLException;
+
+import javax.naming.NamingException;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class WebClientTests {
+
+	private static ChromeOptions options;
+	private WebDriver driver;
+
+	@BeforeAll
+	public static void setDriverPaths() throws NamingException, SQLException {
+
+		if (System.getProperty("webdriver.chrome.driver") == null) {
+			System.setProperty("webdriver.chrome.driver", System.getenv("webdriver.chrome.driver"));
+		}
+
+		options = new ChromeOptions();
+		options.setHeadless(true);
+
+	}
+
+	@BeforeEach
+	public void setUp() throws Exception {
+		driver = new ChromeDriver(options);
+	}
+
+	@AfterEach
+	public void shutdown() {
+		if (driver != null) {
+			driver.close();
+		}
+	}
+
+	@Test
+	public void testGuru99Demo() {
+
+		driver.get("http://demo.guru99.com/test/guru99home/");
+		String title = driver.getTitle();
+		Assert.assertTrue(title.contains("Demo Guru99 Page"));
+	}
+
+}
