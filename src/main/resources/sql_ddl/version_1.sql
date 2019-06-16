72a5ec727a3dedbaec2f3715a80bed50798909b7
Markus Kreth <markus.kreth@web.de>
2019-06-16 18:36:32 +0200

    hibernate and sql dbms independant (a bit more)

--- src/main/resources/sql_ddl/version_1.sql
+++ src/main/resources/sql_ddl/version_1.sql
@@ -1,1 +1,1 @@
-ALTER TABLE `clubinvoice`.`ARTICLE` ADD COLUMN `report` VARCHAR(45) NOT NULL DEFAULT '/reports/mtv_gross_buchholz.jrxml' AFTER `user_id`;
+ALTER TABLE ARTICLE ADD COLUMN report VARCHAR(45) NOT NULL DEFAULT '/reports/mtv_gross_buchholz.jrxml' AFTER user_id;
