ce8a5879eddd8b86f149fca5795769777b264247
Markus Kreth <markus.kreth@web.de>
2020-05-16 20:44:02 +0200

    Encryptor throwiing specified Exception

--- src/main/java/de/kreth/clubinvoice/business/Encryptor.java
+++ src/main/java/de/kreth/clubinvoice/business/Encryptor.java
@@ -19,11 +19,15 @@ import org.apache.commons.codec.binary.Base64;
 public class Encryptor {
 
 	private static final Charset CHARSET = StandardCharsets.UTF_8;
+
 	private static final String SECRET_KEY_1 = "mARku21kRe?thHan";
+
 	private static final String SECRET_KEY_2 = "cLUb4rinVoiCE8s8";
 
 	private IvParameterSpec ivParameterSpec;
+
 	private SecretKeySpec secretKeySpec;
+
 	private Cipher cipher;
 
 	public Encryptor() {
@@ -33,17 +37,17 @@ public class Encryptor {
 			secretKeySpec = new SecretKeySpec(SECRET_KEY_2.getBytes(CHARSET),
 					"AES");
 			cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
-		} catch (UnsupportedEncodingException | NoSuchAlgorithmException
+		}
+		catch (UnsupportedEncodingException | NoSuchAlgorithmException
 				| NoSuchPaddingException e) {
-			throw new RuntimeException(e);
+			throw new EncryptionException("Error initializing Encryptor", e);
 		}
 	}
 
 	/**
 	 * Encrypt the string with this internal algorithm.
 	 *
-	 * @param toBeEncrypt
-	 *                        string object to be encrypt.
+	 * @param toBeEncrypt string object to be encrypt.
 	 * @return returns encrypted string.
 	 * @throws NoSuchPaddingException
 	 * @throws NoSuchAlgorithmException
@@ -53,8 +57,7 @@ public class Encryptor {
 	 * @throws IllegalBlockSizeException
 	 */
 	public String encrypt(String toBeEncrypt)
-			throws NoSuchPaddingException, NoSuchAlgorithmException,
-			InvalidAlgorithmParameterException, InvalidKeyException,
+			throws InvalidAlgorithmParameterException, InvalidKeyException,
 			BadPaddingException, IllegalBlockSizeException {
 		cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
 		byte[] encrypted = cipher.doFinal(toBeEncrypt.getBytes(CHARSET));
@@ -62,13 +65,10 @@ public class Encryptor {
 	}
 
 	/**
-	 * Decrypt this string with the internal algorithm. The passed argument
-	 * should be encrypted using {@link #encrypt(String) encrypt} method of this
-	 * class.
+	 * Decrypt this string with the internal algorithm. The passed argument should be encrypted using
+	 * {@link #encrypt(String) encrypt} method of this class.
 	 *
-	 * @param encrypted
-	 *                      encrypted string that was encrypted using
-	 *                      {@link #encrypt(String) encrypt} method.
+	 * @param encrypted encrypted string that was encrypted using {@link #encrypt(String) encrypt} method.
 	 * @return decrypted string.
 	 * @throws InvalidAlgorithmParameterException
 	 * @throws InvalidKeyException
@@ -81,5 +81,21 @@ public class Encryptor {
 		cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
 		byte[] decryptedBytes = cipher.doFinal(Base64.decodeBase64(encrypted));
 		return new String(decryptedBytes, CHARSET);
+	}
+
+	public class EncryptionException extends RuntimeException {
+
+		public EncryptionException(String message, Throwable cause) {
+			super(message, cause);
+		}
+
+		public EncryptionException(String message) {
+			super(message);
+		}
+
+		public EncryptionException(Throwable cause) {
+			super(cause);
+		}
+
 	}
 }
